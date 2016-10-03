8a6e1103dbeccd7b4e993547af3bb658f9791c53
Tiwaree, Mani <Mani.Tiwaree@mastercard.com>
2016-10-03 18:07:07 -0400

    First Commit

--- pytree.py
+++ pytree.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+import os
+import re
 
+numDir = 0
+numFile = 0
 
-# YOUR CODE GOES here
+#Main function
+def tree(startpath, indentation=""):
+	files = []
+	global numDir
+	global numFile
+	for i in os.listdir(startpath):
+		if(i[0] != "."):
+			files.append(i)
+	files = sorted(files, key=sort)
 
+	for i in range(len(files)):
+		if i == len(files) - 1:
+			print(indentation + "-- "+ files[i])
+		else:
+			print(indentation + "|-- "+ files[i])
 
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+		subdir = startpath + "/" + files[i]
+
+		if os.path.isdir(subdir):
+			numDir += 1
+			if i == len(files) - 1:
+				tree(subdir, indentation + "    ")
+			else:
+				tree(subdir, indentation + "|   ")
+		else:
+			numFile += 1
+
+#Sort 
+def sort(file): 
+    punctuation = '_,;:?"\''
+    return re.sub('[^A-Za-z0-9]+', '', file).lower()
+
+#Printing the tree
+if len(sys.argv) == 1:
+	print (".")
+	tree(".")
+else:
+	print (sys.argv[1])
+	tree(sys.argv[1])
+print("\n" + str(numDir) + " directorties, " + str(numFile) + " files")
